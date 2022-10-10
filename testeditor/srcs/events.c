fbd9fd42fe4da4de7a585cf21b49d3333dfc5c91
Henri Tahvanainen <htahvana@student.hive.fi>
2022-10-10 14:47:20 +0300

    cleaned events.c and added comments

--- testeditor/srcs/events.c
+++ testeditor/srcs/events.c
@@ -6,26 +6,26 @@
 /*   By: htahvana <htahvana@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 14:36:52 by htahvana          #+#    #+#             */
-/*   Updated: 2022/10/10 14:40:17 by htahvana         ###   ########.fr       */
+/*   Updated: 2022/10/10 14:46:54 by htahvana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doomnukem_editor.h"
-
 
 /**
  * converts mouse_pos to world space and snaps to grid
  */
 static void	snap_to_nearest(t_app *app, t_point *mouse_pos, t_vector2 *snap_pos)
 {
-	t_vector2 world_pos;
-	double divider = 1.0f;
+	t_vector2	world_pos;
+	double		divider = 1.0f;
+	double		tmp;
 
 	world_pos.x = app->view_pos.x + (mouse_pos->x / (double)app->surface->w) * app->zoom_area.x;
 	world_pos.y = app->view_pos.y + (mouse_pos->y / (double)app->surface->h) * app->zoom_area.y;
 	snap_pos->x = world_pos.x;
 	snap_pos->y = world_pos.y;
-	double tmp = fabs(fmod(world_pos.x, divider));
+	tmp = fabs(fmod(world_pos.x, divider));
 	if(tmp < (divider / 2))
 	{
 		if(world_pos.x < 0)
@@ -58,27 +58,21 @@ static void	snap_to_nearest(t_app *app, t_point *mouse_pos, t_vector2 *snap_pos)
 }
 
 /**
- * Tracks mouse position and changes player direction accordingly.
+ * Tracks mouse position and saves the snapped location to t_app
  */
 int	events_mouse_track(t_app *app)
 {
-	(void)app;
 	t_point	current_pos;
-	t_vector2 center;
 	
 	SDL_GetMouseState(&current_pos.x, &current_pos.y);
-	
-	center = (t_vector2){0.0,0.0};
 	snap_to_nearest(app, &current_pos, &app->mouse_click);
-	/* pixel_a.x =  fabs(((a->point.x + data->view_pos.x) / data->zoom_area.x ) * data->surface->w);
-	pixel_a.y =  fabs(((a->point.y + data->view_pos.y) / data->zoom_area.y ) * data->surface->h); */
-
-	/* ft_printf("mouse.x=%i, mouse.y=%i, snap.x=%f, snap.y=%f\n" \
-	, current_pos.x, current_pos.y, snap_pos.x, snap_pos.y); */
 	return (0);
 }
 
 
+/**
+ * Mouse scroll zoom
+ */
 int events_mouse_wheel(t_app *app, SDL_Event *event)
 {
 	if(event->wheel.y > 0)
@@ -96,17 +90,6 @@ int events_mouse_wheel(t_app *app, SDL_Event *event)
 		app->view_pos.x /= 2;
 		app->view_pos.y /= 2;
 	}
-
-	/* 	event->area.x += (x / (double)event->data->sizex) * event->size * 0.1;
-		event->area.y += (y / (double)event->data->sizey) * event->size * 0.1;
-		event->size *= 0.9;
-	}
-	if (button == 5)
-	{
-		event->area.x -= (x / (double)event->data->sizex) * event->size * 0.1;
-		event->area.y -= (y / (double)event->data->sizey) * event->size * 0.1;
-		event->size *= 1.1; */
-
 	return (0);
 }
 
