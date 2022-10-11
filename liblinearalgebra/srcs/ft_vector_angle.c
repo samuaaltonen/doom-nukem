95c86e59b51e1ba2f8ae601a636c55bbc1b12f1e
Samu Aaltonen <saaltone@student.hive.fi>
2022-10-11 14:21:41 +0300

    Added ft_vector_angle to liblinearalgebra.

--- liblinearalgebra/srcs/ft_vector_angle.c
+++ liblinearalgebra/srcs/ft_vector_angle.c
@@ -1,21 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_vector_length.c                                 :+:      :+:    :+:   */
+/*   ft_vector_angle.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: saaltone <saaltone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/08 15:07:05 by saaltone          #+#    #+#             */
-/*   Updated: 2022/10/11 12:11:46 by saaltone         ###   ########.fr       */
+/*   Created: 2022/10/11 12:11:15 by saaltone          #+#    #+#             */
+/*   Updated: 2022/10/11 12:13:49 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "liblinearalgebra.h"
 
 /**
- * Returns vector length.
-*/
-double	ft_vector_length(t_vector2 a)
+ * Returns angle between 2 vectors in radians.
+ */
+double	ft_vector_angle(t_vector2 a, t_vector2 b)
 {
-	return (sqrt(a.x * a.x + a.y * a.y));
+	return (acos(ft_vector_dotproduct(a, b)
+			/ (ft_vector_length(a) * ft_vector_length(b))));
 }
