9939d0895c337e6e0a3a62b0ff7f1127569c7b2c
Daniel Palacio <dpalacio@student.hive.fi>
2022-10-14 15:01:43 +0300

    blit_surface() scales the src to the dst rectangle

--- srcs/ui.c
+++ srcs/ui.c
@@ -12,6 +12,9 @@
 
 #include "doomnukem.h"
 
+Uint32	get_pixel(SDL_Surface *surface, int x, int y);
+void	set_pixel(SDL_Surface *surface, int x, int y, Uint32 pixel);
+
 void	render_button(t_app *app)
 {
 	SDL_Surface	*button;
@@ -20,8 +23,8 @@ void	render_button(t_app *app)
 
 	dst.x = 10;
 	dst.y = 10;
-	dst.w = 128;
-	dst.h = 32;
+	dst.w = 512;
+	dst.h = 128;
 	src.x = 0;
 	src.y = 0;
 	src.w = 128;
@@ -39,8 +42,8 @@ void	blit_surface(SDL_Surface *src, t_rect src_rect,
 	int		x;
 	int		y;
 
-	if (dst_rect.w < src_rect.w || dst_rect.h < src_rect.h)
-		exit_error(MSG_ERROR);
+	/*if (dst_rect.w < src_rect.w || dst_rect.h < src_rect.h)
+		exit_error(MSG_ERROR);*/
 	if ((WIN_W - dst_rect.w + dst_rect.x) < 0
 		|| (WIN_H - dst_rect.h + dst_rect.x) < 0)
 		exit_error(MSG_ERROR);
@@ -50,10 +53,10 @@ void	blit_surface(SDL_Surface *src, t_rect src_rect,
 	{
 		while (x < dst_rect.w)
 		{
-			point.x = x + dst_rect.x;
-			point.y = y + dst_rect.y;
-			map_point(dst_rect, src_rect, point);
-			pixel = get_pixel_color(src, point.x + src_rect.x, point.y + src_rect.y);
+			point.x = x;
+			point.y = y;
+			map_point(dst_rect, src_rect, &point);
+			pixel = get_pixel_color(src, point.x, point.y);
 			if ((pixel & 0xFF000000) != 0x00000000)
 				put_pixel_to_surface(dst, x + dst_rect.x,
 					y + dst_rect.y, pixel);
@@ -64,9 +67,8 @@ void	blit_surface(SDL_Surface *src, t_rect src_rect,
 	}
 }
 
-t_point	map_point(t_rect src, t_rect dst, t_point point)
+void	map_point(t_rect src, t_rect dst, t_point *point)
 {
-	point.x = (point.x * (src.w / dst.w) + dst.x);
-	point.y = (point.y * (src.h / dst.h) + dst.w);
-	return (point);
+	point->x = point->x * ((float)dst.w / (float)src.w) + dst.x;
+	point->y = point->y * ((float)dst.h / (float)src.h) + dst.y;
 }
