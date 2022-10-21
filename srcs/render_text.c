d5aaefdb936efbdf1b04485d7a4ac67fd7bcdc4f
Daniel Palacio <dpalacio@student.hive.fi>
2022-10-21 17:55:48 +0300

    text rendering works with new font

--- srcs/render_text.c
+++ srcs/render_text.c
@@ -6,67 +6,89 @@
 /*   By: dpalacio <danielmdc94@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:48:08 by dpalacio          #+#    #+#             */
-/*   Updated: 2022/10/21 16:52:47 by dpalacio         ###   ########.fr       */
+/*   Updated: 2022/10/21 17:54:26 by dpalacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doomnukem.h"
 
-static void		render_char(t_app *app, t_point position, t_rect src);
-static t_rect	get_char(char *str, int c);
+static t_rect	get_rect(int c);
+static void		render_char(t_app *app, t_point *position, t_rect src);
+static t_rect	get_char(char *str, int c, int line);
 
 void	render_text(t_app *app, t_point position, char *text)
 {
-	char	*upcase;
-	char	*lowcase;
 	int		i;
 
-	upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	lowcase = "abcdefghijklmnopqrstuvwxyz";
 	i = 0;
 	while (text[i] != '\0')
 	{
-		if (ft_strchr(upcase, text[i]))
-			render_char(app, position, get_char(upcase, text[i]));
-		position.x += app->assets.font.size - 4;
+//		if (ft_strchr(upcase, text[i]))
+			render_char(app, &position, get_rect(text[i]));
+//		position.x += app->assets.font.size - 4;
 		i++;
 	}
 }
 
-static void	get_char(int c)
+static t_rect	get_rect(int c)
 {
+	t_rect	rect;
 	char	*upcase;
 	char	*lowcase;
+	char	*digits;
+	char	*symbols;
+
+	upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	lowcase = "abcdefghijklmnopqrstuvwxyz";
+	digits = "0123456789";
+	symbols = ".,:;'\"!?-_()/|\\<>\%";
+
+	if (ft_isupper(c))
+		rect = get_char(upcase, c, 0);
+	else if (ft_islower(c))
+		rect = get_char(lowcase, c, 1);
+	else if (ft_isdigit(c))
+		rect = get_char(digits, c, 2);
+	else 
+		rect = get_char(symbols, c, 3);
+		return (rect);
 }
 
-static void	render_char(t_app *app, t_point position, t_rect src)
+static void	render_char(t_app *app, t_point *position, t_rect src)
 {
 	t_rect	dst;
 
-	dst.x = position.x;
-	dst.y = position.y;
-	dst.w = app->assets.font.size;
+	dst.x = position->x;
+	dst.y = position->y;
+	dst.w = src.w * app->assets.font.size / 16;
 	dst.h = app->assets.font.size;
 	blit_surface(app->assets.font.font, &src, app->surface, &dst);
+	position->x += src.w * app->assets.font.size / 16;
 }
 
-static t_rect	get_char(char *str, int c)
+static t_rect	get_char(char *str, int c, int line)
 {
 	t_rect	src;
-	int			font_offset;
-	int			i;
+	int		width;
+	int		i;
 
-	font_offset = 0;
+	if (line == 0)
+		width = 12;
+	else if (line == 1 || line == 2)
+		width = 10;
+	else
+		width = 8;
 	i = 0;
 	while (str[i] != '\0')
 	{
 		if ((str[i]) == c)
-			font_offset = i;
+			src.x = i * 16;
 		i++;
 	}
-	src.x = 16 * font_offset;
-	src.y = 0;
-	src.w = 16;
+	if (c == ' ')
+		src.x = i * 16;
+	src.y = line * 16;
+	src.w = width;
 	src.h = 16;
 	return (src);
 }
--- srcs/title_screen.c
+++ srcs/title_screen.c
@@ -6,7 +6,7 @@
 /*   By: dpalacio <danielmdc94@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 12:05:46 by dpalacio          #+#    #+#             */
-/*   Updated: 2022/10/21 16:46:52 by dpalacio         ###   ########.fr       */
+/*   Updated: 2022/10/21 17:50:37 by dpalacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,13 @@ void	render_menu(t_app *app)
 	//blit_surface(app->assets.button_texture, &src, app->surface, &dst);
 	//change_font(app, 48, 0xFF5F1E02);
 	//render_text(app, (t_point){dst.x + dst.w / 2 - 96, dst.y + dst.h / 2 - 24}, "MENU");
-//	change_font(app, 16, 0xFF111111);
+	change_font(app, 16, 0xFF111111);
 	render_text(app, (t_point){100, 100}, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 	render_text(app, (t_point){100, 120}, "abcdefghijklmnopqrstuvwxyz");
+	render_text(app, (t_point){100, 140}, "0123456789");
+	render_text(app, (t_point){100, 160}, " .,:;'\"!?-_()/|\\<>\%");
+	change_font(app, 32, 0xFF111111);
+	render_text(app, (t_point){100, 180}, "Testing.With <CHARACTERS> and \"diferent\" stuff");
 //	render_text(app, (t_point){260, 180}, "WORKING TITLE!");
 //	change_font(app, 60, 0xFFC10020);
 //	render_text(app, (t_point){224, 84}, "DOOM NUKEM:");
