5516c2368111f8d088219d89ff76adeffa4ff0dc
Samu Aaltonen <saaltone@student.hive.fi>
2022-11-22 14:34:56 +0200

    Fixed norminette issues with liblinearalgebra.

--- liblinearalgebra/srcs/ft_vector_sub.c
+++ liblinearalgebra/srcs/ft_vector_sub.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_vector_sub.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: htahvana <htahvana@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: saaltone <saaltone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 16:05:51 by htahvana          #+#    #+#             */
-/*   Updated: 2022/10/25 16:15:10 by htahvana         ###   ########.fr       */
+/*   Updated: 2022/11/22 14:34:17 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
  */
 t_vector2	ft_vector2_sub(t_vector2 a, t_vector2 b)
 {
-		a.x = a.x - b.x;
-		a.y = a.y - b.y;
-		return (a);
+	a.x = a.x - b.x;
+	a.y = a.y - b.y;
+	return (a);
 }
