2b62db5d8b53d15dc61620a846946f80e1e97f7d
Samu Aaltonen <saaltone@student.hive.fi>
2022-12-05 16:45:39 +0200

    Fixed an issue where animations were able to be created even when animation target pointer already had concurrent animation.

--- srcs/animation_create.c
+++ srcs/animation_create.c
@@ -6,11 +6,33 @@
 /*   By: saaltone <saaltone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 13:07:09 by saaltone          #+#    #+#             */
-/*   Updated: 2022/12/05 14:40:50 by saaltone         ###   ########.fr       */
+/*   Updated: 2022/12/05 16:44:51 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doomnukem.h"
+
+/**
+ * @brief Checks if given pointer already has progressing animation that
+ * modifies its value.
+ * 
+ * @param app 
+ * @param target 
+ * @return t_bool 
+ */
+static t_bool	has_animation(t_app *app, double *target)
+{
+	int	i;
+
+	i = 0;
+	while (i < app->animation_count)
+	{
+		if (app->animations[i].target == target)
+			return (TRUE);
+		i++;
+	}
+	return (FALSE);
+}
 
 /**
  * @brief Adds animation to animation array.
@@ -21,7 +43,8 @@
  */
 t_bool	animation_create(t_app *app, t_animation animation)
 {
-	if (app->animation_count >= MAX_CONCURRENT_ANIMATIONS - 1)
+	if (app->animation_count >= MAX_CONCURRENT_ANIMATIONS - 1
+		|| has_animation(app, animation.target))
 		return (FALSE);
 	app->animations[app->animation_count] = animation;
 	app->animation_count++;
--- srcs/interaction_trigger.c
+++ srcs/interaction_trigger.c
@@ -6,20 +6,29 @@
 /*   By: saaltone <saaltone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 16:06:52 by saaltone          #+#    #+#             */
-/*   Updated: 2022/12/05 16:33:02 by saaltone         ###   ########.fr       */
+/*   Updated: 2022/12/05 16:44:30 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doomnukem.h"
 
-static t_animation	get_height_animation(t_sector *sector, t_interaction *interaction, double *target)
+static t_animation	get_light_animation(t_sector *sector, double variable)
+{
+	return ((t_animation){0.0, ANIMATION_DURATION_LIGHT,
+		(variable - sector->light) / ANIMATION_DURATION_LIGHT,
+		&sector->light});
+}
+
+static t_animation	get_height_animation(t_sector *sector, double variable,
+	double *target)
 {
 	t_animation	animation;
 
 	animation.target = target;
 	animation.progress = 0.0;
-	animation.duration = ANIMATION_DURATION_HEIGHT * fabs(interaction->variable - sector->floor_height);
-	if (interaction->variable - sector->floor_height < 0.0)
+	animation.duration = ANIMATION_DURATION_HEIGHT
+		* fabs(variable - sector->floor_height);
+	if (variable - sector->floor_height < 0.0)
 		animation.increment = -1.0 / ANIMATION_DURATION_HEIGHT;
 	else
 		animation.increment = 1.0 / ANIMATION_DURATION_HEIGHT;
@@ -36,22 +45,24 @@ void	interaction_trigger(t_app *app, int interaction_index)
 {
 	t_interaction	*interaction;
 	t_sector		*target_sector;
-	double			previous_value;
+	double			variable;
 
 	interaction = &app->interactions[interaction_index];
+	variable = interaction->variable;
 	if (interaction->target_sector == -1)
 		return ;
 	target_sector = &app->sectors[interaction->target_sector];
 	if (interaction->event_id == EVENT_CHANGE_LIGHT
-		&& animation_create(app, (t_animation){0.0, ANIMATION_DURATION_LIGHT,
-			(interaction->variable - target_sector->light)
-				/ ANIMATION_DURATION_LIGHT, &target_sector->light}))
-		previous_value = target_sector->light;
-	if ((interaction->event_id == EVENT_CHANGE_FLOOR_HEIGHT || interaction->event_id == EVENT_CHANGE_FLOOR_AND_CEIL_HEIGHT)
-		&& animation_create(app, get_height_animation(target_sector, interaction, &target_sector->floor_height)))
-		previous_value = target_sector->floor_height;
-	if ((interaction->event_id == EVENT_CHANGE_CEIL_HEIGHT || interaction->event_id == EVENT_CHANGE_FLOOR_AND_CEIL_HEIGHT)
-		&& animation_create(app, get_height_animation(target_sector, interaction, &target_sector->ceil_height)))
-		previous_value = target_sector->floor_height;
-	interaction->variable = previous_value;
+		&& animation_create(app, get_light_animation(target_sector, variable)))
+		interaction->variable = target_sector->light;
+	if ((interaction->event_id == EVENT_CHANGE_FLOOR_HEIGHT
+			|| interaction->event_id == EVENT_CHANGE_FLOOR_AND_CEIL_HEIGHT)
+		&& animation_create(app, get_height_animation(target_sector, variable,
+				&target_sector->floor_height)))
+		interaction->variable = target_sector->floor_height;
+	if ((interaction->event_id == EVENT_CHANGE_CEIL_HEIGHT
+			|| interaction->event_id == EVENT_CHANGE_FLOOR_AND_CEIL_HEIGHT)
+		&& animation_create(app, get_height_animation(target_sector, variable,
+				&target_sector->ceil_height)))
+		interaction->variable = target_sector->floor_height;
 }
