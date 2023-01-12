cce6d910ffe751ee1a1188b2800875fd67125831
Samu Aaltonen <saaltone@student.hive.fi>
2023-01-12 16:46:08 +0200

    Added member sector ID and portal sector ID validation for sector validation.

--- srcs/level_validation_sector.c
+++ srcs/level_validation_sector.c
@@ -6,7 +6,7 @@
 /*   By: saaltone <saaltone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 14:45:06 by saaltone          #+#    #+#             */
-/*   Updated: 2023/01/12 16:20:07 by saaltone         ###   ########.fr       */
+/*   Updated: 2023/01/12 16:45:16 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@
  */
 static t_bool	is_convex(t_app *app, int sector_id)
 {
-	int		i;
-	int		next;
+	int	i;
+	int	next;
 
 	i = 0;
 	while (i < app->sectors[sector_id].corner_count)
@@ -31,7 +31,7 @@ static t_bool	is_convex(t_app *app, int sector_id)
 		if (i == app->sectors[sector_id].corner_count - 1)
 			next = 0;
 		if (ft_vector_angle_left(get_wall_vector(app, sector_id, i),
-			get_wall_vector(app, sector_id, next)) < M_PI)
+				get_wall_vector(app, sector_id, next)) < M_PI)
 			return (FALSE);
 		i++;
 	}
@@ -47,7 +47,7 @@ static t_bool	is_convex(t_app *app, int sector_id)
  */
 static t_bool	valid_textures(t_app *app, int sector_id)
 {
-	int		i;
+	int	i;
 
 	i = 0;
 	while (i < app->sectors[sector_id].corner_count)
@@ -56,7 +56,37 @@ static t_bool	valid_textures(t_app *app, int sector_id)
 			|| app->sectors[sector_id].wall_textures[i] >= MAX_TEX_COUNT
 			|| app->sectors[sector_id].wall_decor[i] < -1
 			|| app->sectors[sector_id].wall_decor[i] >= MAX_DECOR_COUNT)
-			return(FALSE);
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
+/**
+ * @brief Check if member sectors and portal sectors have valid IDs.
+ * 
+ * @param app 
+ * @param sector_id 
+ * @return t_bool 
+ */
+static t_bool	valid_linked_sectors(t_app *app, int sector_id)
+{
+	int	i;
+
+	i = 0;
+	while (i < MAX_MEMBER_SECTORS)
+	{
+		if (app->sectors[sector_id].member_sectors[i] < -1
+			|| app->sectors[sector_id].member_sectors[i] >= app->sector_count)
+			return (FALSE);
+		i++;
+	}
+	i = 0;
+	while (i < app->sectors[sector_id].corner_count)
+	{
+		if (app->sectors[sector_id].wall_types[i] < -1
+			|| app->sectors[sector_id].wall_types[i] >= app->sector_count)
+			return (FALSE);
 		i++;
 	}
 	return (TRUE);
@@ -76,7 +106,10 @@ void	level_validation_sectors(t_app *app)
 	{
 		if (app->sectors[i].corner_count > MAX_SECTOR_CORNERS
 			|| !is_convex(app, i)
-			|| !valid_textures(app, i))
+			|| !valid_textures(app, i)
+			|| app->sectors[i].parent_sector >= app->sector_count
+			|| app->sectors[i].parent_sector < -1
+			|| !valid_linked_sectors(app, i))
 			exit_error(MSG_ERROR_VALIDATION_SECTOR);
 		i++;
 	}
