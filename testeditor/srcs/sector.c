fc084e19632d18b26882b994ab88e2a23732bc56
Sarita Sulkumaki <ssulkuma@student.hive.fi>
2023-01-17 16:56:14 +0200

    Editor: Changed default sector textures and height

--- testeditor/srcs/sector.c
+++ testeditor/srcs/sector.c
@@ -6,7 +6,7 @@
 /*   By: ssulkuma <ssulkuma@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 13:36:45 by htahvana          #+#    #+#             */
-/*   Updated: 2023/01/16 14:43:34 by ssulkuma         ###   ########.fr       */
+/*   Updated: 2023/01/17 16:53:17 by ssulkuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -143,7 +143,19 @@ t_bool	complete_sector(t_app *app)
 	app->list_creation = FALSE;
 	new->parent_sector = app->active_sector;
 	add_member_sector(new->parent_sector, new);
-	change_walls_tex(new->wall_list, app->sector_count);
 	change_walls_type(app, new);
+	if (new->parent_sector)
+	{
+		change_walls_tex(new->wall_list, 11);
+		new->ceil_tex = 10;
+		new->floor_tex = 10;
+	}
+	else
+	{
+		change_walls_tex(new->wall_list, 35);
+		new->ceil_height = 3.0;
+		new->ceil_tex = 19;
+		new->floor_tex = 19;
+	}
 	return (0);
 }
