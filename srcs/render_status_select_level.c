ef4793f138628e7b03d5cb996ea36cbde85b4ce1
Daniel Palacio <dpalacio@student.hive.fi>
2023-01-25 13:42:47 +0200

    level selector works for the first 10 levels, the text of all of them still says only "LEVEL 0"

--- srcs/render_status_select_level.c
+++ srcs/render_status_select_level.c
@@ -6,19 +6,22 @@
 /*   By: dpalacio <danielmdc94@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 15:59:18 by dpalacio          #+#    #+#             */
-/*   Updated: 2023/01/24 16:57:17 by dpalacio         ###   ########.fr       */
+/*   Updated: 2023/01/25 13:40:45 by dpalacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doomnukem.h"
 
-static void	select_level_button(t_app *app, t_rect button,
-	void (*f)(t_app *app, int level), int level);
+static void select_level_button(t_app *app, t_rect button,
+								void (*f)(t_app *app, int level), int level);
+static int check_level(int n);
+static void valid_levels(t_app *app);
+static void make_button(t_app *app, int level);
 
-void	render_select_level(t_app *app)
+void render_select_level(t_app *app)
 {
-	t_rect		dst;
-	t_rect		src;
+	t_rect dst;
+	t_rect src;
 
 	SDL_SetRelativeMouseMode(SDL_FALSE);
 	rect_from_surface(app->assets.title_screen_image, &src);
@@ -29,19 +32,14 @@ void	render_select_level(t_app *app)
 	render_text(app, (t_rect){336, 146, 800, 800}, "SELECT LEVEL");
 	render_ui_frame(app, (t_rect){320, 180, 640, 360}, 1, DARK_GREY);
 	render_pointer(app, app->mouse_pos.x, app->mouse_pos.y);
-	select_level_button(app,
-		render_button(app, (t_rect){340, 200, 160, 32}, 1, "LEVEL 0"),
-		import_change_level, 0);
-	select_level_button(app,
-		render_button(app, (t_rect){340, 264, 160, 32}, 1, "LEVEL 1"),
-		import_change_level, 1);
+	valid_levels(app);
 	button_function(app,
 		render_button(app, (t_rect){880, 488, 60, 32}, 1, "Back"),
 		main_menu);
 }
 
-static void	select_level_button(t_app *app, t_rect button,
-	void (*f)(t_app *app, int level), int level)
+static void select_level_button(t_app *app, t_rect button,
+								void (*f)(t_app *app, int level), int level)
 {
 	if (check_mouse(app, button))
 	{
@@ -52,6 +50,62 @@ static void	select_level_button(t_app *app, t_rect button,
 		{
 			f(app, level);
 			start_timer(&app->button_timer, 0.0);
-		}	
+		}
 	}
+}
+
+static void valid_levels(t_app *app)
+{
+	int	i;
+
+	i = 0;
+	while (i < 9)
+	{
+		if (check_level(i))
+			make_button(app, i);
+		i++;
+	}
+}
+
+static int check_level(int n)
+{
+	char *str;
+	char *level;
+	char *file;
+
+	str = ft_strnew(sizeof(char) * 9);
+	str = ft_strcpy(str, "level-");
+	level = ft_itoa(n);
+	file = ft_strcat(str, level);
+	if (access(file, F_OK) == 0)
+	{
+		free(level);
+		free(file);
+		return (1);
+	}
+	free(level);
+	free(file);
+	return (0);
+}
+
+static void make_button(t_app *app, int level)
+{
+	t_rect	rect;
+
+	if (level < 5)
+	{	
+		rect.x = 340;
+		rect.y = 200 + 64 * level;
+	}
+	else if (level >= 5 && level < 10)
+	{	
+		rect.x = 534;
+		rect.y = 200 + 64 * (level - 5);
+	}
+	
+	rect.w = 160;
+	rect.h = 32;
+	select_level_button(app,
+		render_button(app, rect, 1, "LEVEL 0"),
+		import_change_level, level);
 }
