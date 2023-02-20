7bb881b54957f9c21034d976db6cdda55419add2
Samu Aaltonen <saaltone@student.hive.fi>
2023-02-20 17:15:49 +0200

    Made textmodal rendering modal size calculation more precise.

--- srcs/textmodal.c
+++ srcs/textmodal.c
@@ -3,32 +3,90 @@
 /*                                                        :::      ::::::::   */
 /*   textmodal.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: htahvana <htahvana@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: saaltone <saaltone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 13:26:54 by saaltone          #+#    #+#             */
-/*   Updated: 2023/02/15 18:59:39 by htahvana         ###   ########.fr       */
+/*   Updated: 2023/02/20 17:11:35 by saaltone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doomnukem.h"
 
 /**
- * @brief Get the text prompt size object
+ * @brief Get the word index.
+ * 
+ * @param text 
+ * @param index 
+ * @return int 
+ */
+static int	get_modal_word_length(char *text, int index)
+{
+	int	length;
+
+	length = 0;
+	while (text[index] != ' ' && text[index] != '\0')
+	{
+		if (ft_islower(text[index]) || ft_isdigit(text[index]))
+			length += 10;
+		else
+			length += 12;
+		index++;
+	}
+	return (length);
+}
+
+/**
+ * @brief Returns textmodal length.
+ * 
+ * @param app 
+ * @param text 
+ * @return int 
+ */
+static int	get_modal_line_count(char *text)
+{
+	int		i;
+	int		pos;
+	int		lines;
+
+	pos = 0;
+	lines = 1;
+	i = 0;
+	while (text[i] != '\0')
+	{
+		if (i > 0 && text[i - 1] == ' '
+			&& pos + get_modal_word_length(text, i) > WIN_W / 2)
+		{
+			pos = 0;
+			lines++;
+			if (text[i] == ' ')
+				i++;
+		}
+		if (ft_islower(text[i]) || ft_isdigit(text[i]))
+			pos += 10;
+		else
+			pos += 12;
+		i++;
+	}
+	return (lines);
+}
+
+/**
+ * @brief Get the text prompt size.
  * 
  * @param app 
  * @param char_count 
  * @param total 
  * @return t_rect 
  */
-static t_rect	get_text_prompt_size(int char_count, int total)
+static t_rect	get_text_prompt_size(t_app *app, int text_index)
 {
 	t_rect	area;
 
-	area.w = total * 12;
+	area.w = app->text_lengths[text_index] * 12;
 	if (area.w > WIN_W / 2)
 		area.w = WIN_W / 2;
 	area.w += 24;
-	area.h = (char_count * 10 / (WIN_W / 2) + 2) * 20;
+	area.h = (get_modal_line_count(app->texts[text_index]) + 1) * 20;
 	area.x = (WIN_W - area.w) / 2;
 	area.y = WIN_H / 8;
 	return (area);
@@ -51,10 +109,12 @@ void	render_textmodals(t_app *app)
 		char_count = app->text_lengths[app->textmodal.text];
 	if (char_count <= 0)
 		return ;
+	while (app->texts[app->textmodal.text][char_count]
+		&& app->texts[app->textmodal.text][char_count] != ' ')
+		char_count++;
 	char_save = app->texts[app->textmodal.text][char_count];
 	app->texts[app->textmodal.text][char_count] = 0;
-	render_text_prompt(app, get_text_prompt_size(
-			char_count, app->text_lengths[app->textmodal.text]), 1,
+	render_text_prompt(app, get_text_prompt_size(app, app->textmodal.text), 1,
 		app->texts[app->textmodal.text]);
 	app->texts[app->textmodal.text][char_count] = char_save;
 }
